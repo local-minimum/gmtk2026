efa343e072b2d5053ec6796a3160a7c61bc2ea70
Martin Zackrisson <martin.zackrisson@fripost.org>
2026-07-24 10:07:12 +0200

    Add placeholder cutscene one

---
+++ levels/cutscene.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name Cutscene
+
+@export_file("*.tscn") var next_scene: String
+
+var t0: int
+
+func _ready() -> void:
+    t0 = Time.get_ticks_msec()
+
+
+func _input(event: InputEvent) -> void:
+    if Time.get_ticks_msec() < 500:
+        return
+
+    if (event is InputEventKey || event is InputEventJoypadButton || event is InputEventMouseButton) && event.is_pressed():
+        get_tree().change_scene_to_file(next_scene)
